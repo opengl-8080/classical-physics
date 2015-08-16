4f6c5aef9d9e52ffbe22a55254ce2cc5a563390b
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-08-16 21:11:17 +0900

    Physical の default メソッド廃止

--- src/main/java/gl8080/physics/domain/Physical.java
+++ src/main/java/gl8080/physics/domain/Physical.java
@@ -6,18 +6,9 @@ import gl8080.physics.domain.primitive.Velocity;
 
 public interface Physical {
 
-    default Velocity getVelocity() {
-        return Velocity.ZERO;
-    }
-
-    default Point getLocation() {
-        return Point.ORIGIN;
-    }
-
-    default Mass getMass() {
-        return Mass.ZERO;
-    }
-
-    default void setLocation(Point location) {}
-    default void setVelocity(Velocity velocity) {}
+    Velocity getVelocity();
+    Point getLocation();
+    Mass getMass();
+    void setLocation(Point location);
+    void setVelocity(Velocity velocity);
 }
--- src/main/java/gl8080/physics/domain/dummy/DummyObject.java
+++ src/main/java/gl8080/physics/domain/dummy/DummyObject.java
@@ -1,11 +1,39 @@
 package gl8080.physics.domain.dummy;
 
 import gl8080.physics.domain.Physical;
+import gl8080.physics.domain.primitive.Mass;
+import gl8080.physics.domain.primitive.Point;
+import gl8080.physics.domain.primitive.Velocity;
 
 public class DummyObject implements Physical {
     
     @Override
     public String toString() {
         return "DummyObject!!";
+    }
+
+    @Override
+    public Velocity getVelocity() {
+        return Velocity.ZERO;
+    }
+
+    @Override
+    public Point getLocation() {
+        return Point.ORIGIN;
+    }
+
+    @Override
+    public Mass getMass() {
+        return Mass.ZERO;
+    }
+
+    @Override
+    public void setLocation(Point location) {
+        // ignore
+    }
+
+    @Override
+    public void setVelocity(Velocity velocity) {
+        // ignore
     }
 }
